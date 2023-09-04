7cfd1f0f3c78235582af564ab7fd9d611a3f6d6d
Edmundo Batista <eddmundosjb@gmail.com>
2023-09-04 08:40:47 -0300

    feat: trabalhando nas rotas

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// // const knex = require('./database')
+
+// // const app = express();
+
+
+// // var express = require('express');
+// var router = express.Router();
+// const usuariosModel = require('./models/usuariosModel')
+
+// /* GET home page.  */
+
+// router.get('/', async function(req, res, next) {
+
+//     let result = await usuariosModel.getUsers()
+//     console.log(result);
+
+// });
+
+// module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,14 +1,34 @@
 require("dotenv").config();
 
-const express = require('express')
-// const routes = require('./routes')
-
-const app = express();
-
 const port = process.env.SERVER_PORT;
 
+const express = require('express')
+const knex = require('./database')
+const app = express();
+
+const clienteController = require('./controllers/clienteController');
+// const usuariosModel = require('./models/usuariosModel');
+
+// app.use(express.json());
+
+
 app.get("/", function (req, res) {
-        res.send("<h1 style='color: blue;'>Olá, seja Bem-vindo!</h1>")
+        res.send("<h1 style='color: blue;'>Olá Houser, seja Bem-vindo!</h1>")
 })
 
-app.listen(port, () =>  console.log(`Server is running on http://localhost: ${port}`))
+app.get('/clientes', async (req, res) => {
+        await knex.select('nome', 'escala', 'turno')
+        .from('clientes')
+        .where('tipo', 'C')
+        .andWhere('locdial', '<>', '')
+        .orderBy('nome')
+        .then((results) => res.json(results))      
+})
+
+// app.get('/clientesteste', clienteController.getAll);
+// beleza acima tratar depois...
+
+
+
+
+app.listen(port, () => console.log(`Server is running on http://localhost: ${port}`))
